b71818b9b364f6a9214c27e0d29381286b574955
AUMGSOEMOE <soemoe2580aa@gmail.com>
2024-03-31 13:52:28 +0630

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,4 +4,5 @@ export { default as Endemic } from "./birds/module/Endemic.page";
 export { default as Migratory } from "./birds/module/Migratory.page";
 export { default as Native } from "./birds/module/Native.page";
 
+//fome
 export { default as Home } from "./Home.page";
